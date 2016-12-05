46b63c4d5abe7044dac89b468cf4d90afa99648e
tomcharlesosman <tomcharlesosman@gmail.com>
2016-12-05 09:23:44 +0000

    Day1 pt 1 solved

--- days/day1.js
+++ days/day1.js
@@ -1,12 +1,24 @@
 const fs = require('fs');
 const data = fs.readFileSync(__dirname + '/../data/day1.txt', 'utf8');
 
-console.log(data);
-
 function calcBlockDistance () {
+  const steps = data.split(',');
+  let x = 0;
+  let y = 0;
+  let d = [0, 1];
 
+  for (let i = 0; i < steps.length; i++) {
+    const step = steps[i].trim();
+    const direction = step.charAt(0);
+    const distance = +step.substr(1);
+    d = direction === 'L' ? [d[1], -d[0]] : [-d[1], d[0]];
+    x += distance * d[0];
+    y += distance * d[1];
+  }
+  console.log(Math.abs(x) + Math.abs(y));
+  return Math.abs(x) + Math.abs(y);
 }
 
 module.exports = {
   calcBlockDistance
-}
+};
--- spec/day1.spec.js
+++ spec/day1.spec.js
@@ -5,4 +5,10 @@ describe('calcBlockDistance()', () => {
   it('is a function', () => {
     expect(calcBlockDistance).to.be.a('function');
   });
+  it('should return a number', () => {
+    expect(calcBlockDistance()).to.be.a('number');
+  });
+  it('should take one arguement', () => {
+    expect(calcBlockDistance.length).to.equal(1);
+  });
 });
